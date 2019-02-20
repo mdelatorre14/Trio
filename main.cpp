c10d1a11052edefe983ef68d2f963a56cff2d024
Miguel De La Torre <migueldelatorre@wlan-t-184-116-dhcp.CSUChico.EDU>
2019-02-20 11:45:34 -0800

    changed function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void sortDescending(int,int,int);
-void swap(int&,int&);
+//void sortDescending(int,int,int);
+//void swap(int&,int&);
+void sort(int&,int&,int&);
 
 int main()
 {
@@ -12,13 +13,33 @@ int main()
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
 
-  sortDescending(numA, numB, numC);
-  
+  //sortDescending(numA, numB, numC);
+  sort(numA, numB, numC);  
+
   cout<<"From greatest to least, they are: ";
   cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
 
+void sort(int &first, int &second, int &third){
+   int temp; 
+    if(first < second){
+	temp = first;
+	first = second;
+	second = temp;
+    }
+    if(first < third){
+    	temp = first;
+        first = third;
+        third = temp;
+    }
+    if(second < third){
+    	temp = second;
+        second = third;
+        third = temp;
+    }
+}
+/*
 void sortDescending(int first, int second, int third)
 {
   if( first < third )
@@ -40,4 +61,4 @@ void swap(int &first, int &second)
   int temp = first;
   first = second;
   second = temp;
-}
+}*/
